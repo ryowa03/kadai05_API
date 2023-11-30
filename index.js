c0a83465da6f718e2240c65e50f73fa140bdc7e4
ryowa03 <kamijyou76@yahoo.co.jp>
2023-12-01 03:00:45 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+// const canvas = document.getElementById( "canvas" );
+
+
+// const context = canvas.getContext('2d');
+
+// //rect四角形　　　　xywh
+// context.fillRect(100,100,200,200);
+// context.clearRect(100,100,100,100);
+
+
+
+
+
+
